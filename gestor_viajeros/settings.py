9ea90ba3580b7fa66fdfafbf8b042f90bfa8170d
REDACTED <jordanbboy.1996@hotmail.com>
2024-11-07 17:58:10 -0500

    debug True

--- gestor_viajeros/settings.py
+++ gestor_viajeros/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dvcpoih#f%1#wkss*9h6hl!qg%f#7oe67ygq53z(0#b@k5agh('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['viajeros-h8g6fhd0d5gphmg2.eastus-01.azurewebsites.net', 'localhost', '127.0.0.1']
 
