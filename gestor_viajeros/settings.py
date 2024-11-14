4eb40623fc8cd3c0b7ae15e562605c054922457c
REDACTED <jordanbboy.1996@hotmail.com>
2024-11-14 12:41:23 -0500

    ajustes

--- gestor_viajeros/settings.py
+++ gestor_viajeros/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-dvcpoih#f%1#wkss*9h6hl!qg%f#7oe67ygq53z(0#b@k5agh(
 DEBUG = True
 
 ALLOWED_HOSTS = ['viajeros-h8g6fhd0d5gphmg2.eastus-01.azurewebsites.net', 'localhost', '127.0.0.1', 'jordanppk.pythonanywhere.com']
-
 
 # Application definition
 
@@ -43,6 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',  # Habilita la detección de idioma
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,9 +70,7 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'gestor_viajeros.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -84,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -104,18 +100,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'  # Cambia el idioma a español
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'  # Cambia a la zona horaria de Bogotá
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True  # Activa el uso de localización para formatear fechas y números
 
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -124,7 +120,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "viajeros" / "static",  # Ruta a los archivos estáticos
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
